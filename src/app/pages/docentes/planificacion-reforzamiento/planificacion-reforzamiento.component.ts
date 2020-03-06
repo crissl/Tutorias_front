ba4a542d6d12a4b18a160f722f6433c8f4e0bdc8
christian <cac.lopez@yavirac.edu.ec>
2020-03-06 08:03:09 -0500

    maquetacion formulariso docente planificacion reforzamiento y acompanamiento

--- src/app/pages/docentes/planificacion-reforzamiento/planificacion-reforzamiento.component.ts
+++ src/app/pages/docentes/planificacion-reforzamiento/planificacion-reforzamiento.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonalDataService } from 'app/services/personal-data.service';
+import { RestService} from 'app/service/rest.service';
+
 
 @Component({
   selector: 'app-planificacion-reforzamiento',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planificacion-reforzamiento.component.scss']
 })
 export class PlanificacionReforzamientoComponent implements OnInit {
+  constructor(private service: PersonalDataService, private restService: RestService ) { }
+  datosGuardar : any;
+  ncr:any;
 
-  constructor() { }
-
+  cedula="1725412306";
   ngOnInit() {
-  }
+   }
+   id:any
+   procesaPropagar(data){
+     this.id=data[0].pidm
+     //console.log(data[0].pidm)
+   }
+   tema:any={
+     tema:""
+   }
 
+   
+   listarNrc(){
+     this.restService.findData(this.id).subscribe(
+       data => {
+
+       }
+     )
+   }
 }
